7e240bfb0dfdc90386099110eaa0572a87ff6358
Daniele Guglietti <gugliettidaniele@gmail.com>
2022-01-28 19:48:18 +0100

    feat(client): added command registration

--- src/client/Client.ts
+++ src/client/Client.ts
@@ -13,7 +13,52 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import Discord from "discord.js";
+import Discord, { Collection } from "discord.js";
+import { Routes } from "discord-api-types/v9";
+import { ICommand } from "../models/Command";
+import { REST } from "@discordjs/rest";
+
+type ReadonlyCollection<K, V> = Readonly<Collection<K, V>>;
+
+type ClientOptions = Discord.ClientOptions & {
+    token: string;
+};
 
 export default class Client extends Discord.Client {
+    #commands: Collection<string, ICommand>;
+
+    #rest: REST;
+
+    constructor(options: ClientOptions) {
+        super(options);
+
+        this.#commands = new Collection();
+    
+        this.#rest = new REST({ version: "9" })
+            .setToken(options.token);
+    }
+
+    /**
+     * The commands commands collection.
+     */
+    get commands(): ReadonlyCollection<string, ICommand> {
+        return this.#commands;
+    }
+
+    /**
+     * Register a new command.
+     * @param {string} name - The command name.
+     * @param {ICommand} command - The command object.
+     */
+    async register(name: string, command: ICommand) {
+        this.#commands.set(name, command);
+
+        if (!this.isReady()) {
+            throw new Error("Client is not ready!");
+        }
+
+        this.#rest.post(Routes.applicationCommands(this.user.id), {
+            body: command.data
+        });
+    }
 }
